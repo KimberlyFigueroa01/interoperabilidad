bcd861cb861bb9ba8af581284def4df9ac56087c
KimberlyFigueroa21 <kimfigueroa210004@gmail.com>
2026-03-07 15:53:00 -0500

    estructura inicial del proyecto

---
+++ src/main/java/edu/uptc/software/ProyectInteroperability.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.uptc.software;
+
+/**
+ *
+ * @author Kimberly Figueroa
+ */
+public class ProyectInteroperability {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
