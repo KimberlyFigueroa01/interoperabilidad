656244ce8552ff73861c2ca6fee3a348f9ba653e
AlissonPaez <alisson.paez@uptc.edu.co>
2026-03-08 16:43:35 -0500

    Clase adaptador para el punto 4

---
+++ src/main/java/edu/uptc/software/adaptador/ProveedorPagoAdaptador.java
@@ -0,0 +1,36 @@
+package edu.uptc.software.adaptador;
+
+import java.util.Map;
+
+import edu.uptc.software.empresa.ServicioPago;
+import edu.uptc.software.proveedor.PaymentProvider;
+import edu.uptc.software.proveedor.User;
+
+public class ProveedorPagoAdaptador implements ServicioPago {
+
+    private PaymentProvider externalProvider;
+    private String moneda;
+
+    public ProveedorPagoAdaptador(PaymentProvider externalProvider, String moneda) {
+        this.externalProvider = externalProvider;
+        this.moneda = moneda;
+    }
+
+    @Override
+    public RespuestaPago procesarPago(String clienteId, long monto) {
+        User userExterno = new User(clienteId, "Nombre del Cliente");
+        
+        Map<String, Object> respuesta = externalProvider.executeTransaction(userExterno, monto, moneda);
+
+        String estado = (String) respuesta.get("status");
+        String codigoAutorizacion = (String) respuesta.get("authId");
+
+
+        return new RespuestaPago(codigoAutorizacion, estado);
+    }
+
+
+
+    
+    
+}
